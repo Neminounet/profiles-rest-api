6df983e0759e1fc2bbe37db9244062217141ee9a
Neminounet <neminounet@gmail.com>
2023-02-08 21:42:21 +0100

    gros ratage sur views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,37 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import render
 
-from profiles_api.serializers import HelloSerializer
-
-class HelloApiView(APIView):
-    """Test API View"""
-    serializer_class = HelloSerializer
-
-    def get(self, request, format=None):
-        """Returns a list of APIView features"""
-        an_apiview = [
-            "Uses HTTP methodes as function (get, post, patch, put, delete)",
-            "Is similar to a traditional Django View",
-            "Gives you the most control over your application logic",
-            "Is mapped manually to URLs",
-        ]
-
-        return Response({'message' : "hello", "an_apiview" : an_apiview})
-
-    def post(self, request):
-        """Create a hello message with our name"""
-        serializer = self.serializer_class(data=request.data)
-
-        if serializer.is_valid():
-            name = serializer.validated_data.get("name")
-            message = f"Hello {name}"
-            return Response({"message" : message})
-        else: 
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-    
+# Create your views here.
